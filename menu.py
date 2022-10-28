fe0b274f6c653598d82ad7829b8f0973580567ca
andmansim <91722324+andmansim@users.noreply.github.com>
2022-10-28 15:31:36 +0200

    creo lo básico del menu

---
+++ menu.py
@@ -0,0 +1,47 @@
+import helpers
+import principal as p
+
+def iniciar():
+     while True:
+        helpers.limpiar_pantalla()
+        
+        print("========================")
+        print(" BIENVENIDO AL Manager ")
+        print("========================")
+        print("[1] ")
+        print("[2] ")
+        print("[3]  ")
+        print("[4]  ")
+        print("[5]  ")
+        print("[6]  ")
+        print("========================")
+        
+        opcion = input("> ")
+        helpers.limpiar_pantalla()
+
+        if opcion == '1':
+            print("...\n")
+            
+                            
+        if opcion == '2':
+            print("...\n")
+            
+        
+        if opcion == '3':
+            print("...\n")
+            # Comprobación de DNI válido
+           
+        
+        if opcion == '4':
+            print("...\n")
+           
+                
+        if opcion == '5':
+            print("...\n")
+            
+            
+        if opcion == '6':
+            print("Saliendo...\n")
+            break
+       
+        input("\nPresiona ENTER para continuar...")
