25e37e4a9d333babf1f7c086bb701a1f273b335d
andmansim <91722324+andmansim@users.noreply.github.com>
2022-10-29 20:35:22 +0200

    primera parte

--- ejercicio2.py
+++ ejercicio2.py
@@ -3,30 +3,35 @@ Realiza el código para calcular el determinante de una matriz cuadrada de [3 x 
 regla de Sarrus de forma recursiva y de forma iterativa.
 '''
 '''
-matrz = [[1, 2, 3], 
-         [2, 4, 5], 
-         [6, 8, 6]]
+matriz = [[1, 2, 3], 
+          [2, 3, 4], 
+          [5, 6, 7]]
 '''
 
 
 def det_iterativa(matriz, i):
-    j = i+1
-    h = j +1
     if i >= 3:
         i = 0
+    
+    j = i+1
     if j >= 3:
         j = 0
+        
+    h = j +1
     if  h>= 3:
         h = 0
     pri = matriz[0][i] * matriz[1][j] * matriz[2][h]
     print(pri)
-    return pri
+    return pri, i
 
 matriz = [[1, 2, 3], 
           [2, 3, 4], 
           [5, 6, 7]]
 
 numero = 0
+suma = 0
 while numero != 3:
-    uno= det_iterativa(matriz, numero)
+    uno, numero = det_iterativa(matriz, numero)
     numero = numero + 1
+    suma = suma + uno
+    print(suma)
