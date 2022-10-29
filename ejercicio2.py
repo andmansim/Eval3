a849c9b1000b90fe8059c8e1da4e99471f446d93
andmansim <91722324+andmansim@users.noreply.github.com>
2022-10-29 21:10:53 +0200

    creo q puede ser asi

--- ejercicio2.py
+++ ejercicio2.py
@@ -7,19 +7,28 @@ matriz = [[1, 2, 3],
           [2, 3, 4], 
           [5, 6, 7]]
 '''
-
-
-def det_iterativa(matriz, i):
+def comprobacion(i, j, h):
     if i >= 3:
         i = 0
-    
-    j = i+1
+        return i
     if j >= 3:
         j = 0
-        
-    h = j +1
+        return j
     if  h>= 3:
         h = 0
+        return h
+    
+def parametros(i, j, h):
+    
+    i = comprobacion(i, j, h)
+    j = i+1
+    j = comprobacion(i, j, h)   
+    h = j +1
+    h = comprobacion(i, j, h)
+    
+    return i, j, h
+
+def det_iterativa(matriz, i, j, h):
     pri = matriz[0][i] * matriz[1][j] * matriz[2][h]
     print(pri)
     return pri, i
@@ -28,20 +37,24 @@ matriz = [[1, 2, 3],
           [2, 3, 4], 
           [5, 6, 7]]
 
-numero = 0
+i = 0
+j = 0
+h = 0
 suma = 0
 resta = 0
 contador = 0
 while contador != 6:
     
     if contador < 3:
-        uno, numero = det_iterativa(matriz, numero)
+        i, j, h = parametros(i, j, h)
+        uno, i = det_iterativa(matriz, i, j, h)
         suma = suma + uno
     else:
-        dos, numero = det_iterativa(matriz, numero)
+        i, j, h = parametros(h, j, i)
+        dos, i = det_iterativa(matriz, 2, 1, 0)
         resta = resta + 1
     contador = contador + 1
-    numero = numero + 1
+    i = i + 1
 
 
 print(suma, resta)
