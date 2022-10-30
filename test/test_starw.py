85cf50650080c346c4769c3efcdf0cf3e81e052c
andmansim <91722324+andmansim@users.noreply.github.com>
2022-10-30 20:25:48 +0100

    ya va

--- test/test_starw.py
+++ test/test_starw.py
@@ -3,20 +3,21 @@ from ejercicio3 import *
 from helpers import *
 
 
-
+vehiculos = helpers.leer('vehicles.csv')
+helpers.limpiar(vehiculos)
+lista = filtrar(vehiculos)
+halcon = añadir_datos('Halcón Milenario', 34.37, 4,4)
+estrella = añadir_datos('Estrella de la Muerte', 80000, 1200000 , 825984)
+lista.append(halcon)
+lista.append(estrella)
+nombre = ordenar(lista, 'nombre')
+largo = ordenar(lista, 'largo')
+tripulacion = ordenar(lista, 'tripulacion')
+pasajeros1 = pasajeros[0:5]
 
 class TestDatabase(unittest.TestCase):
     
-    vehiculos = helpers.leer('vehicles.csv')
-    helpers.limpiar(vehiculos)
-    lista = filtrar(vehiculos)
-    halcon = añadir_datos('Halcón Milenario', 34.37, 4,4)
-    estrella = añadir_datos('Estrella de la Muerte', 80000, 1200000 , 825984)
-    lista.append(halcon)
-    lista.append(estrella)
-    nombre = ordenar(lista, 'nombre')
-    largo = ordenar(lista, 'largo')
-    tripulacion = ordenar(lista, 'tripulacion')
+    
     
     def test_nave_mayor_trip(self):
         self.assertEqual(naves(lista, [tripulacion[0]], 'tripulacion', 'nombre')  , ['Estrella de la Muerte'])
