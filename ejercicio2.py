e6f7f52d2884ce94d60ccb2f590bc7cdd718c141
andmansim <91722324+andmansim@users.noreply.github.com>
2022-10-30 21:43:48 +0100

    cambios

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,3 +1,7 @@
+matriz = [[1, 2, 3], 
+                    [2, 3, 4], 
+                  [5, 6, 7]]
+
 def comprobacion(i, j, h):
     if i >= 3:
         i = 0
@@ -33,3 +37,23 @@ def det_iterativa(matriz, i, j, h):
     pri = matriz[0][i] * matriz[1][j] * matriz[2][h]
     return pri
 
+def iterar(i,j,h, resta, suma, contador):
+    while contador != 6:
+
+        if contador < 3:
+
+            uno = det_iterativa(matriz, i, j, h)
+            suma = suma + uno
+            i, j, h = parametros(i, j, h, contador)
+
+        else:
+            if contador == 3:
+                h, j, i = i, j, h
+
+            dos = det_iterativa(matriz, i, j, h)
+            resta = resta + dos
+            i, j, h = parametros(i, j, h, contador)
+        contador = contador + 1
+
+    total = suma - resta
+    return total
--- test/test_det.py
+++ test/test_det.py
@@ -2,37 +2,19 @@ import unittest
 from ejercicio2 import *
 from helpers import *
 
-matriz = [[1, 2, 3], 
-                    [2, 3, 4], 
-                  [5, 6, 7]]
-#FORMA ITERATIVA
+
+'''#FORMA ITERATIVA
 i = 0
 j = 1
 h = 2
 suma = 0
 resta = 0
-contador = 0
-def iterar(i,j,h, resta, suma, contador):
-    while contador != 6:
+contador = 0'''
 
-        if contador < 3:
-
-            uno = det_iterativa(matriz, i, j, h)
-            suma = suma + uno
-            i, j, h = parametros(i, j, h, contador)
-
-        else:
-            if contador == 3:
-                h, j, i = i, j, h
-
-            dos = det_iterativa(matriz, i, j, h)
-            resta = resta + dos
-            i, j, h = parametros(i, j, h, contador)
-        contador = contador + 1
-
-    total = suma - resta
-    return total
 
 class TestDatabase(unittest.TestCase):
     def test_total(self):
-        self.assertAlmostEqual(iterar(0, 1, 2, 0, 0), 0)
+        self.assertAlmostEqual(iterar(0, 1, 2, 0, 0, 0), 0)
+        
+if __name__ == '__main__':
+    unittest.main()
