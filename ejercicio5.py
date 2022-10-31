fd42f3db15a48974480429e37e8ec4180f501ad2
andmansim <91722324+andmansim@users.noreply.github.com>
2022-10-31 13:34:06 +0100

    vemos como unirlo

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,8 +1,11 @@
 mensaje_cifrar = input('Introduce el mensaje: ')
 
-m1 = mensaje_cifrar.lower()
+m1 = mensaje_cifrar.lower()#todo a minúscula
+m1 = m1.split()#quitamos espacios
 a = 'abcdefghijklmnñopqrstuvwxyz'
+mensaje = []
 def cifrar(texto,clave):
+    mensaje = []
     longitud=len(texto) #Calculamos la longitud del texto para recorrerlo
     for i in range (0,longitud): #Bucle para recorrer el texto
         for j in range (0,26): #Bucle para recorrer el Abecedario
@@ -11,16 +14,22 @@ def cifrar(texto,clave):
                 nuevo=j+int(clave) #Me desplazo en el abecedario en función a la clave
                 if nuevo <26:
                     b = a[j + 1:nuevo + 1]
-                    cod.append(b[::-1])
+                    
                     print( a[j + 1:nuevo + 1])
                     print(print(cod))
                     print(texto[i])
                 if nuevo >25 : #En el caso de que me pase del abecedario, calculo el modulo
                     print(a[nuevo%26])
+                    b = a[nuevo%26 : nuevo%26 + 8]
                     print(texto[i])
+                cod.append(b[::-1])
+                mensaje = mensaje + cod
+                print(mensaje)
             else:
                 print('NO')
-    return
+    for h in mensaje:
+        v = ''.join(h)
+    return v 
 cifrar(m1, 8)
 
 
