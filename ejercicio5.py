953f108a60a295444ccff54f209bad5b23a3451e
andmansim <91722324+andmansim@users.noreply.github.com>
2022-10-31 13:05:25 +0100

    mayoúculas y minúculas

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,3 +1,6 @@
 mensaje_cifrar = input('Introduce el mensaje: ')
 if mensaje_cifrar == mensaje_cifrar.upper():
-    
+    a = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
+else: 
+    a = 'abcdefghijklmnñopqrstuvwxyz'
+
