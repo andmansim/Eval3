e251b896a9e445b23f962d081e02d91f869fd28d
andmansim <91722324+andmansim@users.noreply.github.com>
2022-10-31 13:15:36 +0100

    viendo como cifrar

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,6 +1,21 @@
 mensaje_cifrar = input('Introduce el mensaje: ')
-if mensaje_cifrar == mensaje_cifrar.upper():
-    a = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
-else: 
-    a = 'abcdefghijklmnñopqrstuvwxyz'
+
+m1 = mensaje_cifrar.lower()
+
+def cifrar(texto,clave):
+    longitud=len(texto) #Calculamos la longitud del texto para recorrerlo
+    for i in range (0,longitud): #Bucle para recorrer el texto
+        for j in range (0,26): #Bucle para recorrer el Abecedario
+            if texto[i]==a[j]: #comparo las letras una por una
+                nuev = 
+                pos_abc=j+int(clave) #Me desplazo en el abecedario en función a la clave
+                if pos_abc <26:
+                    print(a[pos_abc])
+                if pos_abc >25 : #En el caso de que me pase del abecedario, calculo el modulo
+                    print(a[pos_abc%26])
+            else:
+                print('a')
+    return
+cifrar(m1, 8)
+
 
